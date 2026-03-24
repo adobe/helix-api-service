df64e97cf9dd8c64f954f28155573f43f0ceb799
Dominique Pfister <dpfister@adobe.com>
2026-03-24 17:42:52 +0100

    chore(test): use .now to fix returned `created` and `lastModified` dates

--- test/config/admin-config-store.test.js
+++ test/config/admin-config-store.test.js
@@ -46,12 +46,6 @@ describe('Admin Config Store Tests', () => {
       purgeInfos = opts;
     });
     sandbox.stub(discover, 'projectChanged').callsFake((context, oldConfig, newConfig, org, site) => {
-      if (newConfig) {
-        // eslint-disable-next-line no-param-reassign
-        delete newConfig.created;
-        // eslint-disable-next-line no-param-reassign
-        delete newConfig.lastModified;
-      }
       projectInfo = {
         oldConfig, newConfig, org, site,
       };
@@ -188,13 +182,14 @@ describe('Admin Config Store Tests', () => {
         .reply(201);
 
       const cs = new AdminConfigStore('org', 'sites', 'site');
+      cs.now = new Date(Date.UTC(2024, 0, 1));
+
       const { context } = setupTest('/org/sites/site/config.json', {
         data: copyDeep(SITE_CONFIG),
       });
       const result = await cs.fetchCreate(context);
 
       assert.strictEqual(result.status, 201);
-
       assert.deepStrictEqual(purgeInfos, {
         org: 'org',
         site: 'site',
@@ -208,7 +203,11 @@ describe('Admin Config Store Tests', () => {
       });
       assert.deepStrictEqual(projectInfo, {
         oldConfig: null,
-        newConfig: SITE_CONFIG,
+        newConfig: {
+          ...SITE_CONFIG,
+          created: cs.now.toISOString(),
+          lastModified: cs.now.toISOString(),
+        },
         org: 'org',
         site: 'site',
       });
@@ -239,6 +238,92 @@ describe('Admin Config Store Tests', () => {
         'x-error': 'Error creating config: access for org/site to drive.google.com denied by tenant lock',
         'x-error-code': 'AEM_BACKEND_CONFIG_CREATE',
       });
+    });
+
+    it('returns 409 if config already exists in storage', async () => {
+      nock.config()
+        .headObject('/orgs/org/profiles/profile1.json')
+        .reply(200);
+
+      const cs = new AdminConfigStore('org', 'profiles', 'profile1');
+      const { context } = setupTest('/org/profiles/profile1.json');
+      const result = await cs.fetchCreate(context);
+
+      assert.strictEqual(result.status, 409);
+    });
+
+    it('returns 400 for invalid config', async () => {
+      nock.config()
+        .headObject('/orgs/org/sites/site.json')
+        .reply(404)
+        .getObject('/orgs/org/profiles/default.json')
+        .reply(404);
+
+      const cs = new AdminConfigStore('org', 'sites', 'site');
+      const { context } = setupTest('/org/sites/site/config.json', {
+        data: { invalid: 'config' },
+      });
+      const result = await cs.fetchCreate(context);
+
+      assert.strictEqual(result.status, 400);
+      assert.deepStrictEqual(result.headers.plain(), {
+        'cache-control': 'no-store, private, must-revalidate',
+        'content-type': 'text/plain; charset=utf-8',
+        'x-error': 'Error creating config: data must have required properties: [content, code]; data must NOT have additional properties',
+        'x-error-code': 'AEM_BACKEND_CONFIG_CREATE',
+      });
+    });
+  });
+
+  describe('update', () => {
+    it('profile in storage', async () => {
+      nock.versions('profiles', 'org', 'profile1');
+      nock.config()
+        .getObject('/orgs/org/profiles/profile1.json')
+        .reply(404)
+        .putObject('/orgs/org/profiles/profile1.json')
+        .reply(201);
+
+      const cs = new AdminConfigStore('org', 'profiles', 'profile1');
+      cs.now = new Date(Date.UTC(2024, 0, 1));
+
+      const { context } = setupTest('/org/profiles/profile1.json', {
+        data: {
+          title: 'my profile',
+        },
+      });
+      const result = await cs.fetchUpdate(context);
+
+      assert.strictEqual(result.status, 200);
+      assert.deepStrictEqual(await result.json(), {
+        title: 'my profile',
+        version: 1,
+        created: cs.now.toISOString(),
+        lastModified: cs.now.toISOString(),
+      });
+    });
+
+    it('list of tokens in site', async () => {
+      nock.versions('sites', 'org', 'site1');
+      nock.config()
+        .getObject('/orgs/org/sites/site1.json')
+        .reply(200, SITE_CONFIG)
+        .getObject('/orgs/org/profiles/default.json')
+        .reply(404)
+        .putObject('/orgs/org/sites/site1.json')
+        .reply(201);
+
+      const cs = new AdminConfigStore('org', 'sites', 'site1');
+      const { context } = setupTest('/org/sites/site1/config.json', {
+        data: {},
+      });
+      const result = await cs.fetchUpdate(context, 'tokens');
+
+      assert.strictEqual(result.status, 200);
+      const res = await result.json();
+      assert.ok(res.created);
+      assert.ok(res.id);
+      assert.ok(res.value);
     });
   });
 });
